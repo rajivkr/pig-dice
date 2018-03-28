e99faed04f16b01bf41d22ef4ff6a09d5d477dd9
Rajiv <rajiv270992@gmail.com>
2018-03-28 22:49:49 +0530

    second commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+
+var app = express();
+app.use(express.static(path.join('public')));
+
+app.listen(port, ()=>{
+   console.log(`Server started up st ${port}`);
+});
