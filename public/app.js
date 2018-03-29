4f3f28aa16376c1522b0e04325765bb8a6f2d874
Rajiv <rajiv270992@gmail.com>
2018-03-29 09:14:29 +0530

    add js variables

--- public/app.js
+++ public/app.js
@@ -8,3 +8,12 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+let scores,roundScore,activePlayer,dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+dice = Math.floor(Math.random()*6)+1;
+
+document.querySelector('#score-0').textContent = scores[0].toString();
+document.querySelector('#score-1').textContent = scores[1].toString();
