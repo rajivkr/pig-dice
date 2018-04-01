4509a0abe8f9862091f57048d61526766ce049e2
Rajiv <rajiv270992@gmail.com>
2018-04-01 19:25:01 +0530

    replacing lite-server with gulp

--- public/app.js
+++ public/app.js
@@ -12,8 +12,10 @@ let scores,roundScore,activePlayer,dice;
 
 scores = [0,0];
 roundScore = 0;
-activePlayer = 0;
+activePlayer = 1;
 dice = Math.floor(Math.random()*6)+1;
 
-document.querySelector('#score-0').textContent = scores[0].toString();
-document.querySelector('#score-1').textContent = scores[1].toString();
+document.querySelector(`#current-${activePlayer}`).textContent = dice;
+//document.querySelector(`#current-${activePlayer}`).innerHTML = `<em> ${dice} </em>`;
+var x = document.querySelector(`#score-0`).textContent;
+document.querySelector(`.dice`).style.display = 'none';
